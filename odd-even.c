909ad526f54af9590f6717a70bbbc0a3135165e3
AayuAmor <aayuamor@gmail.com>
2025-02-13 03:39:33 +0000

    Add: code for even odd checking

---
+++ odd-even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a Number:");
+    scanf("%d",&num);
+    if(num % 2 == 0){
+        printf("The Number is Even.");
+    }
+    else{
+        printf("The Number is Odd\n");
+    }
+    return 0;
+}
